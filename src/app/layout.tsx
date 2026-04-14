52ddff22403734b17391db86ab8577c5d212ef41
Tao Ho <vantao.dev@gmail.com>
2026-04-14 16:31:32 +0700

    enhance sidebar to overlay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body className="bg-gray-50 text-gray-900">
         <div className="flex">
           <Sidebar />
-          <main className="min-h-screen flex-1 p-6">{children}</main>
+          <main className="min-h-screen flex-1 pl-48 p-24">{children}</main>
         </div>
       </body>
     </html>
